b3e0dd6b9e59068a5de589a834062ccffd82a11e
Jordan Chapelle <jordan.chapelle@infomaniak.com>
2026-01-21 18:27:14 +0100

    refactor: Move UIModels to CorUI

---
+++ AuthenticatorCoreUI/Preview/PreviewHelper.swift
@@ -0,0 +1,55 @@
+/*
+ Infomaniak Authenticator - iOS App
+ Copyright (C) 2026 Infomaniak Network SA
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import Foundation
+
+public enum PreviewHelper {
+    public static let sampleUIAccount = UIAccount(
+        id: "1",
+        name: "Laura Snow",
+        email: "laura.snow@ik.me",
+        profilePictureURL: nil,
+        status: .protected
+    )
+
+    public static let sampleUIAccounts = [
+        UIAccount(
+            id: "1",
+            name: "Laura Snow",
+            email: "laura.snow@ik.me",
+            profilePictureURL: URL(
+                string: "https://media.4-paws.org/4/9/8/9/49896113ec1edb7553e53df93fec214bfba35e4d/VIER%20PFOTEN_2015-04-27_010-3369x2246-3246x2246-1920x1329.jpg"
+            ),
+            status: .protected
+        ),
+        UIAccount(
+            id: "2",
+            name: "John Appleseed",
+            email: "john.apple@etik.com",
+            profilePictureURL: nil,
+            status: .unprotected
+        ),
+        UIAccount(
+            id: "3",
+            name: "Paul Anderson",
+            email: "paul.anderson@infomaniak.com",
+            profilePictureURL: nil,
+            status: .protected
+        )
+    ]
+}
---
+++ AuthenticatorCoreUI/UIModels/UIAccount.swift
@@ -0,0 +1,59 @@
+/*
+ Infomaniak Authenticator - iOS App
+ Copyright (C) 2026 Infomaniak Network SA
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import AuthenticatorResources
+import SwiftUI
+
+public struct UIAccount: Identifiable, Hashable, Sendable {
+    public let id: String
+    public let name: String
+    public let email: String
+    public let profilePictureURL: URL?
+    public let status: Status
+
+    public init(id: String, name: String, email: String, profilePictureURL: URL?, status: Status) {
+        self.id = id
+        self.name = name
+        self.email = email
+        self.profilePictureURL = profilePictureURL
+        self.status = status
+    }
+
+    public enum Status: Sendable {
+        case protected
+        case unprotected
+
+        public var color: Color {
+            switch self {
+            case .protected:
+                .Token.Status.valid
+            case .unprotected:
+                .Token.Status.warning
+            }
+        }
+
+        public var icon: Image {
+            switch self {
+            case .protected:
+                AuthenticatorResourcesAsset.Images.shieldCheck.swiftUIImage
+            case .unprotected:
+                AuthenticatorResourcesAsset.Images.shieldWarning.swiftUIImage
+            }
+        }
+    }
+}
