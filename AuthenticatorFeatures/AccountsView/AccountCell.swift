53276bde7054edc7b108cacb30f5f76742cc04a7
Jordan Chapelle <jordan.chapelle@infomaniak.com>
2026-01-21 18:32:38 +0100

    feat: Account cell

---
+++ AuthenticatorFeatures/AccountsView/AccountCell.swift
@@ -0,0 +1,40 @@
+/*
+ Infomaniak Authenticator - iOS App
+ Copyright (C) 2026 Infomaniak Network SA
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import AuthenticatorCoreUI
+import InfomaniakCoreSwiftUI
+import NukeUI
+import SwiftUI
+
+struct AccountCell: View {
+    let account: UIAccount
+
+    var body: some View {
+        HStack {
+            AccountLabel(account: account, size: .small)
+                .frame(maxWidth: .infinity, alignment: .leading)
+
+            account.status.icon
+                .foregroundStyle(account.status.color)
+        }
+    }
+}
+
+#Preview {
+    AccountCell(account: PreviewHelper.sampleUIAccount)
+}
