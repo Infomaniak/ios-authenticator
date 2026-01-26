3ac2bc93a5ac4709ad674dd82e00c5bf99670130
Jordan Chapelle <jordan.chapelle@infomaniak.com>
2026-01-26 15:32:52 +0100

    fix: Custom toolbar button style

--- AuthenticatorFeatures/AccountsView/AccountsView.swift
+++ AuthenticatorFeatures/AccountsView/AccountsView.swift
@@ -43,6 +43,7 @@ public struct AccountsView: View {
                     } label: {
                         AuthenticatorLabel(\.addAccountButton, iconKey: \.plus)
                     }
+                    .primaryActionToolbarButtonStyle()
                 }
             }
         }
