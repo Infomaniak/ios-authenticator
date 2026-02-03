0e8e2252d835ab0f7c0baaba18c1d4d97f05e9d4
Jordan Chapelle <jordan.chapelle@infomaniak.com>
2026-02-03 15:50:06 +0100

    fix: Remove old unused imports

--- AuthenticatorFeatures/SettingsView/ContactSupportSettingsViews/ContactSupportActionsView.swift
+++ AuthenticatorFeatures/SettingsView/ContactSupportSettingsViews/ContactSupportActionsView.swift
@@ -16,7 +16,6 @@
  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import AuthenticatorCore
 import AuthenticatorCoreUI
 import AuthenticatorResources
 import DesignSystem
