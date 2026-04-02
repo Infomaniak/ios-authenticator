8d0295c56aee82451023d4268f129fda275a696e
Ambroise Decouttere <ambroise.decouttere@infomaniak.com>
2026-04-02 10:59:57 +0200

    feat: Sentry 9.8
    # Conflicts:
    #	Tuist/Package.swift

--- Tuist/Package.swift
+++ Tuist/Package.swift
@@ -35,8 +35,8 @@ let package = Package(
     name: "Authenticator",
     dependencies: [
         .package(url: "https://github.com/airbnb/lottie-spm.git", .upToNextMajor(from: "4.5.1")),
-        .package(url: "https://github.com/getsentry/sentry-cocoa", .upToNextMajor(from: "8.0.0")),
-        .package(url: "https://github.com/Infomaniak/ios-core", .upToNextMajor(from: "18.8.0")),
+        .package(url: "https://github.com/getsentry/sentry-cocoa", .upToNextMajor(from: "9.8.0")),
+        .package(url: "https://github.com/Infomaniak/ios-core", branch: "feat/sentry"),
         .package(url: "https://github.com/Infomaniak/ios-core-ui", .upToNextMajor(from: "24.3.0")),
         .package(url: "https://github.com/Infomaniak/ios-create-account", .upToNextMajor(from: "23.2.0")),
         .package(url: "https://github.com/Infomaniak/ios-device-check", .upToNextMajor(from: "1.1.1")),
