5bc9d70e3d69f33baac104bdeb0deb1ccc744e4d
Philippe Weidmann <philippe.weidmann@infomaniak.com>
2026-07-31 08:53:30 +0200

    fix: Replace asset

--- AuthenticatorCoreUI/UIModels/UIAccount.swift
+++ AuthenticatorCoreUI/UIModels/UIAccount.swift
@@ -16,6 +16,7 @@
  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+import AuthenticatorCore
 import AuthenticatorResources
 import CoreAuthenticator
 import SwiftUI
@@ -64,7 +65,7 @@ public struct UIAccount: Identifiable, Hashable, Sendable {
             case .partiallyProtected:
                 AuthenticatorResourcesAsset.Images.shieldExclamationmark.swiftUIImage
             case .loggedOut, .loginFailed, .loginFailedRetriable:
-                AuthenticatorResourcesAsset.Images.circleSlash.swiftUIImage
+                AuthenticatorResourcesAsset.Images.circleExclamationmark.swiftUIImage
             }
         }
 
